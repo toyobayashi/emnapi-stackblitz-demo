a73c497b45095cac49e5b0dc1e3677e7b2856259
toyobayashi <lifenglin314@outlook.com>
2024-01-07 18:52:32 +0800

    upgrade emnapi to 0.45.0

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 mkdir -p build/wasm32-unknown-emscripten
 mkdir -p build/wasm32-wasi-threads
 
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -DEMNAPI_WORKER_POOL_SIZE=4 -G Ninja -H. -Bbuild/wasm32-unknown-emscripten
+emcmake cmake -DCMAKE_BUILD_TYPE=Release -DEMNAPI_WORKER_POOL_SIZE=4 -DEMNAPI_FIND_NODE_ADDON_API=ON -G Ninja -H. -Bbuild/wasm32-unknown-emscripten
 cmake --build build/wasm32-unknown-emscripten
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$WASI_SDK_PATH/share/cmake/wasi-sdk-pthread.cmake \
+      -DEMNAPI_FIND_NODE_ADDON_API=ON \
       -DWASI_SDK_PREFIX=$WASI_SDK_PATH \
       -DCMAKE_BUILD_TYPE=Release \
       -G Ninja -H. -Bbuild/wasm32-wasi-threads
